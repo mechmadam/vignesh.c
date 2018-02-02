62c71f32adeb869454f25b5384a2dfbf9aeecb8d
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-02 13:26:43 +0530

    odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+printf("ENTER THE NUMBER a");
+scanf("%d",&a);
+if(a%2==0);
+{
+printf("even number");
+}
+else 
+{
+printf("odd number");
+}
+getch();
+return 0;
+}
